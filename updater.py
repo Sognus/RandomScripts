057ceeb528ad8bf37e66a96987645bd47f6dbe5f
Sognus <msognus@gmail.com>
2019-03-21 01:55:51 +0100

    Replaced Fstrings, Cleaned code
    Debian distributions mostly supports older versions of python3. Those versions do not support Fstrings. Replaced Fstrings. Also adjusted code a little and removed debug messages.

--- updater.py
+++ updater.py
@@ -6,19 +6,17 @@ PAPER_API_URL = "https://papermc.io/api/v1/paper"
 PAPER_MC_VERSION = "1.13.2"
 
 if __name__ == '__main__':
-    req = requests.get("https://papermc.io/api/v1/paper/1.13.2/")
+    req = requests.get("{}/{}/".format(PAPER_API_URL, PAPER_MC_VERSION))
     if req.status_code == 200:
         latest = req.json()["builds"]["latest"]
         files = sorted(glob.glob("paper-*"), reverse=True)
         current = files[0][6:-4] if len(files) > 0 else None
-        print(latest)
-        print(current)
         if latest != current:
-            print(f"Server update found: {current} -> {latest}")
+            print("Server update found: {} -> {}".format(current, latest))
             latest.replace("-", "/");
-            print(f"{PAPER_API_URL}/{PAPER_MC_VERSION}/{latest}/download")
-            new = requests.get(f"{PAPER_API_URL}/{PAPER_MC_VERSION}/{latest}/download")
-            with open(f"paper-{latest}.jar", "wb") as f:
+            print("{}/{}/{}/download".format(PAPER_API_URL, PAPER_MC_VERSION, latest))
+            new = requests.get("{}/{}/{}/download".format(PAPER_API_URL, PAPER_MC_VERSION, latest))
+            with open("paper-{}.jar".format(latest), "wb") as f:
                 f.write(new.content)
             if current is not None:
                 os.remove(files[0])
